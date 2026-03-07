fa0881ff7f2e248988dee46a907f62f65d000bc6
shrish98 <your-shrishtitomar300@gmail.com>
2026-03-07 17:11:15 +0530

    fix inngest serve import

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,4 +1,5 @@
-import { serve, inngest } from "@/lib/inngest/client";
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
